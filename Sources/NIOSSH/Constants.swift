ff2093af578650c93bb9025df188b49e79119191
Artem Redkin <aredkin@apple.com>
2019-12-11 13:29:33 +0000

    move version to a separate file

---
+++ Sources/NIOSSH/Constants.swift
@@ -0,0 +1,17 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftNIO open source project
+//
+// Copyright (c) 2019 Apple Inc. and the SwiftNIO project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftNIO project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+enum Constants {
+    public static let version = "SSH-2.0-SwiftNIOSSH_1.0"
+}
