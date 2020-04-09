0a0404f3bf07152d1a15dd8d518d6cb5da69fb3f
Cory Benfield <lukasa@apple.com>
2020-04-09 13:30:06 +0100

    Provide SSH server example.
    Motivation:
    
    A useful example for how to build SSH servers.
    
    Modifications:
    
    Adds a server example.
    
    Result:
    
    We have a server example.

---
+++ Sources/NIOSSHServer/ExecHandler.swift
@@ -0,0 +1,154 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftNIO open source project
+//
+// Copyright (c) 2020 Apple Inc. and the SwiftNIO project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftNIO project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import Foundation
+import Dispatch
+import NIO
+import NIOSSH
+import NIOFoundationCompat
+
+enum SSHServerError: Error {
+    case invalidCommand
+    case invalidDataType
+}
+
+final class ExampleExecHandler: ChannelDuplexHandler {
+    typealias InboundIn = SSHChannelData
+    typealias InboundOut = ByteBuffer
+    typealias OutboundIn = ByteBuffer
+    typealias OutboundOut = SSHChannelData
+
+    let queue = DispatchQueue(label: "background exec")
+    var process: Process?
+    var environment: [String: String] = [:]
+
+    func handlerAdded(context: ChannelHandlerContext) {
+        context.channel.setOption(ChannelOptions.allowRemoteHalfClosure, value: true).whenFailure { error in
+            context.fireErrorCaught(error)
+        }
+    }
+
+    func channelInactive(context: ChannelHandlerContext) {
+        self.queue.sync {
+            if let process = self.process, process.isRunning {
+                process.terminate()
+            }
+        }
+        context.fireChannelInactive()
+    }
+
+    func userInboundEventTriggered(context: ChannelHandlerContext, event: Any) {
+        switch event {
+        case let event as SSHChannelRequestEvent.ExecRequest:
+            self.exec(event, channel: context.channel)
+
+        case let event as SSHChannelRequestEvent.EnvironmentRequest:
+            self.queue.sync {
+                environment[event.name] = event.value
+            }
+
+        default:
+            context.fireUserInboundEventTriggered(event)
+        }
+    }
+
+    func channelRead(context: ChannelHandlerContext, data: NIOAny) {
+        let data = self.unwrapInboundIn(data)
+
+        guard case .byteBuffer(let bytes) = data.data else {
+            fatalError("Unexpected read type")
+        }
+
+        guard case .channel = data.type else {
+            context.fireErrorCaught(SSHServerError.invalidDataType)
+            return
+        }
+
+        context.fireChannelRead(self.wrapInboundOut(bytes))
+    }
+
+    func write(context: ChannelHandlerContext, data: NIOAny, promise: EventLoopPromise<Void>?) {
+        let data = self.unwrapOutboundIn(data)
+        context.write(self.wrapOutboundOut(SSHChannelData(type: .channel, data: .byteBuffer(data))), promise: promise)
+    }
+
+    private func exec(_ event: SSHChannelRequestEvent.ExecRequest, channel: Channel) {
+        // Kick this off to a background queue
+        self.queue.async {
+            do {
+                // We're not a shell, so we just do our "best".
+                let executable = URL(fileURLWithPath: "/usr/local/bin/bash")
+                let process = Process()
+                process.executableURL = executable
+                process.arguments = ["-c", event.command]
+                process.terminationHandler = { process in
+                    // The process terminated. Check its return code, fire it, and then move on.
+                    let rcode = process.terminationStatus
+                    channel.triggerUserOutboundEvent(SSHChannelRequestEvent.ExitStatus(exitStatus: Int(rcode))).whenComplete { _ in
+                        channel.close(promise: nil)
+                    }
+                }
+
+                let inPipe = Pipe()
+                let outPipe = Pipe()
+                let errPipe = Pipe()
+
+                process.standardInput = inPipe
+                process.standardOutput = outPipe
+                process.standardError = errPipe
+                process.environment = self.environment
+
+                let (ours, theirs) = GlueHandler.matchedPair()
+                try channel.pipeline.addHandler(ours).wait()
+
+                _ = try NIOPipeBootstrap(group: channel.eventLoop)
+                    .channelOption(ChannelOptions.allowRemoteHalfClosure, value: true)
+                    .channelInitializer { pipeChannel in
+                        return pipeChannel.pipeline.addHandler(theirs)
+                    }.withPipes(inputDescriptor: outPipe.fileHandleForReading.fileDescriptor, outputDescriptor: inPipe.fileHandleForWriting.fileDescriptor).wait()
+
+                // Ok, great, we've sorted stdout and stdin. For stderr we need a different strategy: we just park a thread for this.
+                DispatchQueue(label: "stderrorwhatever").async {
+                    while true {
+                        let data = errPipe.fileHandleForReading.readData(ofLength: 1024)
+
+                        guard data.count > 0 else {
+                            // Stderr is done
+                            return
+                        }
+
+                        var buffer = channel.allocator.buffer(capacity: data.count)
+                        buffer.writeBytes(data)
+                        channel.write(SSHChannelData(type: .stdErr, data: .byteBuffer(buffer)), promise: nil)
+                    }
+                }
+
+                if event.wantReply {
+                    channel.triggerUserOutboundEvent(ChannelSuccessEvent(), promise: nil)
+                }
+
+                try process.run()
+                self.process = process
+            } catch {
+                if event.wantReply {
+                    channel.triggerUserOutboundEvent(ChannelFailureEvent()).whenComplete { _ in
+                        channel.close(promise: nil)
+                    }
+                } else {
+                    channel.close(promise: nil)
+                }
+            }
+        }
+    }
+}
---
+++ Sources/NIOSSHServer/main.swift
@@ -0,0 +1,74 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftNIO open source project
+//
+// Copyright (c) 2020 Apple Inc. and the SwiftNIO project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftNIO project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import Dispatch
+import Crypto
+import NIO
+import NIOSSH
+
+// This file contains an example NIO SSH server. It's not intended for production use, it's not secure,
+// but it's a good example of how to
+final class ErrorHandler: ChannelInboundHandler {
+    typealias InboundIn = Any
+
+    func errorCaught(context: ChannelHandlerContext, error: Error) {
+        print("Error in pipeline: \(error)")
+        context.close(promise: nil)
+    }
+}
+
+/// This delegate simply accepts a hardcoded password. For obvious reasons, don't deploy this!
+final class HardcodedPasswordDelegate: NIOSSHServerUserAuthenticationDelegate {
+    var supportedAuthenticationMethods: NIOSSHAvailableUserAuthenticationMethods {
+        return .password
+    }
+
+    func requestReceived(request: NIOSSHUserAuthenticationRequest, responsePromise: EventLoopPromise<NIOSSHUserAuthenticationOutcome>) {
+        guard request.username == "nio", case .password(let passwordRequest) = request.request else {
+            responsePromise.succeed(.failure)
+            return
+        }
+
+        if passwordRequest.password == "gottagofast" {
+            responsePromise.succeed(.success)
+        } else {
+            responsePromise.succeed(.failure)
+        }
+    }
+}
+
+let group = MultiThreadedEventLoopGroup(numberOfThreads: 1)
+defer {
+    try! group.syncShutdownGracefully()
+}
+
+func sshChildChannelInitializer(_ channel: Channel) -> EventLoopFuture<Void> {
+    return channel.pipeline.addHandler(ExampleExecHandler())
+}
+
+// We need a host key. For now, generate it dynamically.
+let hostKey = NIOSSHHostPrivateKey(ed25519Key: .init())
+
+let bootstrap = ServerBootstrap(group: group)
+    .childChannelInitializer { channel in
+        channel.pipeline.addHandlers([NIOSSHHandler(role: .server(hostKey), allocator: channel.allocator, clientUserAuthDelegate: nil, serverUserAuthDelegate: HardcodedPasswordDelegate(), inboundChildChannelInitializer: sshChildChannelInitializer(_:)), ErrorHandler()])
+    }
+    .serverChannelOption(ChannelOptions.socket(SocketOptionLevel(SOL_SOCKET), SO_REUSEADDR), value: 1)
+    .serverChannelOption(ChannelOptions.socket(SocketOptionLevel(IPPROTO_TCP), TCP_NODELAY), value: 1)
+
+
+let channel = try bootstrap.bind(host: "0.0.0.0", port: 2222).wait()
+
+// Run forever
+try channel.closeFuture.wait()
