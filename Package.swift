1a915a324afefd4031e396e81a0e210178621be8
Artem Redkin <aredkin@apple.com>
2026-04-07 17:22:31 +0100

    Allow latest swift-crypto major release (#233)

--- Package.swift
+++ Package.swift
@@ -38,7 +38,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.81.0"),
-        .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0"..<"4.0.0"),
+        .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0"..<"5.0.0"),
         .package(url: "https://github.com/apple/swift-atomics.git", from: "1.0.2"),
     ],
     targets: [
