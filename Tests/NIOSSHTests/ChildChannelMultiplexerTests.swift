1e4fbad5b42e8439e4b6538491ba532f0fe89526
Raphael Hiesgen <r_hiesgen@apple.com>
2026-07-21 09:28:16 +0200

    Cleanup comment

--- Tests/NIOSSHTests/ChildChannelMultiplexerTests.swift
+++ Tests/NIOSSHTests/ChildChannelMultiplexerTests.swift
@@ -1550,7 +1550,7 @@ final class ChildChannelMultiplexerTests: XCTestCase {
         )
         XCTAssertEqual(harness.flushedMessages.count, 1)
 
-        // The default window size is based on Constants. Sadly, we need a buffer that size.
+        // The default window size is 8 MB. Sadly, we need a buffer that size.
         let defaultWindowSize = Constants.defaultMaximumChannelPacketSize * Constants.channelWindowSizePacketMultiple
         let buffer = ByteBuffer.bigBuffer
 
